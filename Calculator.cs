3a4f8fd1291954d0623b4c4f27e149edb3cf89a5
AbdurrahmanRidho <97325407+AbdurrahmanRidho@users.noreply.github.com>
2022-03-17 17:50:44 +0700

    Update Calculator.cs

--- Calculator.cs
+++ Calculator.cs
@@ -7,6 +7,8 @@ class calculator
 		
 		Console.WriteLine("Hasil Penambahan: {0} + {1} = {2}", a, b, Penambahan(a, b));
 		Console.WriteLine("Hasil Pengurangan: {0} - {1} = {2}", a, b, Pengurangan(a, b));
+		Console.WriteLine("Hasil Perkalian: {0} * {1} = {2}", a, b, Perkalian(a, b));
+		Console.WriteLine("Hasil Pembagian: {0} / {1} = {2}", a, b, Pembagian(a, b));
 		
 		Console.WriteLine("\nTekan sembarang key untuk keluar");
 		Console.ReadKey();
@@ -20,5 +22,13 @@ class calculator
 	static int Pengurangan(int a, int b)
 	{
 		return a - b;	
+	}
+	static int Perkalian(int a, int b)
+	{
+		return a * b;	
+	}
+	static int Pembagian(int a, int b)
+	{
+		return a / b;	
 	}
 }
